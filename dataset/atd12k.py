376ac2d3762d65267ed9b8e347773f9720199b61
shenjiaming <shenjiaming@rd.netease.com>
2022-09-02 03:19:07 +1000

    a

---
+++ dataset/atd12k.py
@@ -0,0 +1,103 @@
+import os
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+import random
+
+class ATD12k(Dataset):
+    def __init__(self, data_root, is_training , input_frames="1357", mode='mini'):
+        """
+        Creates a Vimeo Septuplet object.
+        Inputs.
+            data_root: Root path for the Vimeo dataset containing the sep tuples.
+            is_training: Train/Test.
+            input_frames: Which frames to input for frame interpolation network.
+        """
+        self.data_root = data_root
+        self.image_root = os.path.join(self.data_root, 'sequences')
+        self.training = is_training
+        self.inputs = input_frames
+
+        dirs = os.listdir(self.data_root)
+        data_list = []
+        for d in dirs:
+            img0 = os.path.join(self.data_root, d, 'frame1.jpg')
+            img1 = os.path.join(self.data_root, d, 'frame3.jpg')
+            gt = os.path.join(self.data_root, d, 'frame2.jpg')
+            points = os.path.join(self.data_root, d, 'inter.jpg')
+            data_list.append([img0, img1, gt, points, d])
+
+        self.data_list = data_list
+
+        if self.training:
+            self.transforms = transforms.Compose([
+                transforms.RandomCrop(256),
+                transforms.RandomHorizontalFlipVideo(),
+                transforms.RandomVerticalFlipVideo(0.5),
+                # transforms.ColorJitter(0.05, 0.05, 0.05, 0.05),
+                transforms.ToTensor()
+            ])
+        else:
+            self.transforms = transforms.Compose([
+                transforms.ToTensor()
+            ])
+
+    def __getitem__(self, index):
+
+        imgpaths = [self.data_list[index][0], self.data_list[index][1], self.data_list[index][2], self.data_list[index][3]]
+
+        # Load images
+        images = [Image.open(pth) for pth in imgpaths]
+
+        ## Select only relevant inputs
+        # inputs = [int(e)-1 for e in list(self.inputs)]
+        # inputs = inputs[:len(inputs)//2] + [3] + inputs[len(inputs)//2:]
+        # images = [images[i] for i in inputs]
+        # imgpaths = [imgpaths[i] for i in inputs]
+        # Data augmentation
+        if self.training:
+            seed = random.randint(0, 2**32)
+            images_ = []
+            for img_ in images:
+                random.seed(seed)
+                images_.append(self.transforms(img_))
+            images = images_
+            # Random Temporal Flip
+            if random.random() >= 0.5:
+                images = images[::-1]
+                imgpaths = imgpaths[::-1]
+            gt = images[2]
+            images = images[:1]
+
+            return images, gt
+        else:
+            T = self.transforms
+            images = [T(img_) for img_ in images]
+
+            gt = images[2]
+            images = images[:1]
+            imgpath = self.data_list[index][4]
+
+            return images, gt, imgpath
+
+    def __len__(self):
+        if self.training:
+            return len(self.trainlist)
+        else:
+            return len(self.testlist)
+            # return 1
+
+def get_loader(mode, data_root, batch_size, shuffle, num_workers, test_mode=None):
+    if mode == 'train':
+        is_training = True
+    else:
+        is_training = False
+    dataset = ATD12k(data_root, is_training=is_training)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=True)
+
+
+if __name__ == "__main__":
+
+    dataset = ATD12k("/Users/shenjiaming/Documents/2022 S1/ELEC4712/MySuperGlue/atd12k_points", is_training=True)
+    print(dataset[0])
+    dataloader = DataLoader(dataset, batch_size=100, shuffle=False, num_workers=32, pin_memory=True)
