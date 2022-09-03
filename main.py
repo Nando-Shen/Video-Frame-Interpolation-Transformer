62cfa6b17d921530d57a6a791b02d38a0c5a81c4
shenjiaming <shenjiaming@rd.netease.com>
2022-09-04 06:37:47 +1000

    add cross atten

--- main.py
+++ main.py
@@ -81,8 +81,6 @@ def train(args, epoch):
 
         # Build input batch
         images = [img_.to(device) for img_ in images]
-        print(images[0].shape)
-        print(images[1].shape)
 
         # Forward
         optimizer.zero_grad()
