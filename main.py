5692100399d86e5ebe123f700c2eac6b1f3720a3
shenjiaming <shenjiaming@rd.netease.com>
2022-09-04 18:59:10 +1000

    add cross atten

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def train(args, epoch):
         # Forward
         optimizer.zero_grad()
         if args.model == 'VFI':
-            out, flow_list = model(images[0], images[1], points)
+            out, flow_list = model(images[0], images[1], images[2])
         else:
             out_ll, out_l, out = model(images)
 
@@ -123,7 +123,7 @@ def test(args, epoch):
 
             images = [img_.to(device) for img_ in images]
             if args.model == 'VFI':
-                out, flow_list = model(images[0], images[1], points)
+                out, flow_list = model(images[0], images[1], images[2])
             else:
                 out = model(images)
 
