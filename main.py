bee3d803383f42ef91c5e19d080c3aac20e6d156
shenjiaming <shenjiaming@rd.netease.com>
2022-09-06 18:29:29 +1000

    add cross atten

--- main.py
+++ main.py
@@ -170,9 +170,9 @@ def adjust_learning_rate(optimizer, epoch):
 
 """ Entry Point """
 def main(args):
-    # load_checkpoint(args, model, optimizer, save_loc+'/checkpoint.pth')
-    # test_loss, psnr, ssim = test(args, args.start_epoch)
-    # print(psnr)
+    load_checkpoint(args, model, optimizer, save_loc+'/checkpoint.pth')
+    test_loss, psnr, ssim = test(args, args.start_epoch)
+    print(psnr)
 
     best_psnr = 0
     for epoch in range(args.start_epoch, args.max_epoch):
