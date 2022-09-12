370b8a963ee138255559f03570a38a2370d4bd79
shenjiaming <shenjiaming@rd.netease.com>
2022-09-13 00:15:42 +1000

    add cross atten

--- main.py
+++ main.py
@@ -133,7 +133,8 @@ def test(args, epoch):
 
             images = [img_.to(device) for img_ in images]
             if args.model == 'VFI':
-                out = model(images[0], images[1], images[2])
+                with autocast():
+                    out = model(images[0], images[1], images[2])
             else:
                 out = model(images)
 
@@ -149,7 +150,8 @@ def test(args, epoch):
             losses['total'].update(loss.item())
 
             # Evaluate metrics
-            myutils.eval_metrics(out, gt, psnrs, ssims)
+            with autocast():
+                myutils.eval_metrics(out, gt, psnrs, ssims)
 
     return losses['total'].avg, psnrs.avg, ssims.avg
 
@@ -188,14 +190,14 @@ def main(args):
     for epoch in range(args.start_epoch, args.max_epoch):
         adjust_learning_rate(optimizer, epoch)
         start_time = time.time()
-        # train(args, epoch)
-        #
-        # torch.save({
-        #     'epoch': epoch,
-        #     'state_dict': model.state_dict(),
-        #     'optimizer': optimizer.state_dict(),
-        #     'lr': optimizer.param_groups[-1]['lr']
-        # }, os.path.join(save_loc, 'checkpoint.pth'))
+        train(args, epoch)
+
+        torch.save({
+            'epoch': epoch,
+            'state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'lr': optimizer.param_groups[-1]['lr']
+        }, os.path.join(save_loc, 'checkpoint.pth'))
 
         test_loss, psnr, ssim = test(args, epoch)
 
