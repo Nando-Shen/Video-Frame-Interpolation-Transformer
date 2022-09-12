f12e54ef7958b024852477ecac9d9efdf5b1caf6
shenjiaming <shenjiaming@rd.netease.com>
2022-09-13 00:38:43 +1000

    add cross atten

--- model/VFIformer_arch.py
+++ model/VFIformer_arch.py
@@ -479,7 +479,7 @@ class VFIformerSmall(nn.Module):
         c = 24
         height = args.crop_size
         width = args.crop_size
-        window_size = 4
+        window_size = 8
         embed_dim = 136
 
         self.flownet = IFNet()
@@ -491,7 +491,7 @@ class VFIformerSmall(nn.Module):
 
         self.transformer = TFModel(img_size=(height, width), in_chans=2*c, out_chans=4, fuse_c=c,
                                           window_size=window_size, img_range=1.,
-                                          depths=[[2, 2], [2, 2], [2, 2], [1, 1]],
+                                          depths=[[3, 3], [3, 3], [3, 3], [1, 1]],
                                           embed_dim=embed_dim, num_heads=[[2, 2], [2, 2], [2, 2], [2, 2]], mlp_ratio=2,
                                           resi_connection='1conv',
                                           use_crossattn=[[[False, False, False, False], [False, False, False, False]], \
