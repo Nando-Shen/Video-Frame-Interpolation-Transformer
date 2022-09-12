d3b46b121277a68599e6d097e241a87cec611a06
shenjiaming <shenjiaming@rd.netease.com>
2022-09-13 00:44:16 +1000

    add cross atten

--- model/VFIformer_arch.py
+++ model/VFIformer_arch.py
@@ -480,7 +480,7 @@ class VFIformerSmall(nn.Module):
         height = args.crop_size
         width = args.crop_size
         window_size = 8
-        embed_dim = 160
+        embed_dim = 180
 
         self.flownet = IFNet()
         self.refinenet = FlowRefineNet_Multis_Simple(c=c, n_iters=1)
