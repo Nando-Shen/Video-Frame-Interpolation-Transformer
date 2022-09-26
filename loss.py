f80b34ff768186ab75a5be18d6c9bb76781ef670
shenjiaming <shenjiaming@rd.netease.com>
2022-09-26 18:27:24 +1000

    add cross atten

--- loss.py
+++ loss.py
@@ -1,7 +1,6 @@
 # Sourced from https://github.com/myungsub/CAIN/blob/master/loss.py, who sourced from https://github.com/thstkdgus35/EDSR-PyTorch/tree/master/src/loss
 # Added Huber loss in addition.
-
-
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -293,6 +292,8 @@ class Loss(nn.modules.loss._Loss):
                 loss_function = pytorch_msssim.SSIM(val_range=1.)
             elif loss_type.find('GAN') >= 0:
                 loss_function = Adversarial(args, loss_type)
+            elif loss_type == 'Ternary':
+                loss_function = Ternary()
 
             self.loss.append({
                 'type': loss_type,
@@ -336,3 +337,41 @@ class Loss(nn.modules.loss._Loss):
 
 
         return loss, losses
+
+
+class Ternary(nn.Module):
+    def __init__(self):
+        super(Ternary, self).__init__()
+        patch_size = 7
+        out_channels = patch_size * patch_size
+        self.w = np.eye(out_channels).reshape(
+            (patch_size, patch_size, 1, out_channels))
+        self.w = np.transpose(self.w, (3, 2, 0, 1))
+        # self.w = torch.tensor(self.w).float().to(device)
+
+    def transform(self, img):
+        patches = F.conv2d(img, self.w, padding=3, bias=None)
+        transf = patches - img
+        transf_norm = transf / torch.sqrt(0.81 + transf**2)
+        return transf_norm
+
+    def rgb2gray(self, rgb):
+        r, g, b = rgb[:, 0:1, :, :], rgb[:, 1:2, :, :], rgb[:, 2:3, :, :]
+        gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+        return gray
+
+    def hamming(self, t1, t2):
+        dist = (t1 - t2) ** 2
+        dist_norm = torch.mean(dist / (0.1 + dist), 1, True)
+        return dist_norm
+
+    def valid_mask(self, t, padding):
+        n, _, h, w = t.size()
+        inner = torch.ones(n, 1, h - 2 * padding, w - 2 * padding).type_as(t)
+        mask = F.pad(inner, [padding] * 4)
+        return mask
+
+    def forward(self, img0, img1):
+        img0 = self.transform(self.rgb2gray(img0))
+        img1 = self.transform(self.rgb2gray(img1))
+        return self.hamming(img0, img1) * self.valid_mask(img0, 1)
