36b1281bbea2e6b545244e0df4189d47cb2d9368
shenjiaming <shenjiaming@rd.netease.com>
2022-09-26 17:45:57 +1000

    add cross atten

--- model/VFIformer_arch.py
+++ model/VFIformer_arch.py
@@ -482,7 +482,7 @@ class VFIformerSmall(nn.Module):
         embed_dim = 160
 
         self.flownet = IFNet()
-        self.refinenet = FlowRefineNet_Multis_Simple(c=c, n_iters=1)
+        self.refinenet = FlowRefineNet_Multis(c=c, n_iters=1)
         self.fuse_block = nn.Sequential(nn.Conv2d(12, 2*c, 3, 1, 1),
                                          nn.LeakyReLU(negative_slope=0.2, inplace=True),
                                          nn.Conv2d(2*c, 2*c, 3, 1, 1),
