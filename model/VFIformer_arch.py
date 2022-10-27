db40916510ff6a97872238dbbb90ff425b02645f
shenjiaming <shenjiaming@rd.netease.com>
2022-10-28 09:49:43 +1100

    add cross atten

--- model/VFIformer_arch.py
+++ model/VFIformer_arch.py
@@ -512,7 +512,7 @@ class VFIformerSmall(nn.Module):
                                                       [[False, False, False, False], [False, False, False, False]], \
                                                       [[False, False, False, False], [False, False, False, False]], \
                                                       [[False, False, False, False], [False, False, False, False]]])
-        self.cross_tran = TFCModel(img_size=(height, width), in_chans=9, out_chans=3, fuse_c=c,
+        self.cross_tran = TFCModel(img_size=(height, width), in_chans=3, out_chans=3, fuse_c=c,
                                           window_size=window_size, img_range=1.,
                                           depths=[[3, 3], [3, 3], [3, 3], [1, 1]],
                                           embed_dim=embed_dim, num_heads=[[2, 2], [2, 2], [2, 2], [2, 2]], mlp_ratio=2,
