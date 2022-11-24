4f57cf1247701b8c972e3f63a44f454ea7cd7010
shenjiaming <shenjiaming@rd.netease.com>
2022-11-25 00:39:53 +0800

    add cross atten

--- model/VFIformer_arch.py
+++ model/VFIformer_arch.py
@@ -486,8 +486,8 @@ class VFIformerSmall(nn.Module):
 
         # self.flownet = IFNet()
         # self.refinenet = FlowRefineNet_Multis_Simple(c=c, n_iters=1)
-        self.flownet = IFNet()
-        self.refinenet = FlowRefineNet_Multis(c=c, n_iters=1)
+        # self.flownet = IFNet()
+        # self.refinenet = FlowRefineNet_Multis(c=c, n_iters=1)
         self.fuse_block = nn.Sequential(nn.Conv2d(9, 2*c, 3, 1, 1),
                                          nn.LeakyReLU(negative_slope=0.2, inplace=True),
                                          nn.Conv2d(2*c, 2*c, 3, 1, 1),
@@ -560,7 +560,7 @@ class VFIformerSmall(nn.Module):
 
     def forward(self, img0, img1, points):
         B, _, H, W = img0.size()
-        imgs = torch.cat((img0, img1), 1)
+        # imgs = torch.cat((img0, img1), 1)
 
         # if flow_pre is not None:
         #     flow = flow_pre
@@ -574,30 +574,30 @@ class VFIformerSmall(nn.Module):
         # warped_img0 = warp(img0, flow[:, :2])
         # warped_img1 = warp(img1, flow[:, 2:])
 
-        flow, _ = self.flownet(imgs)
-        flow, _, _ = self.refinenet(img0, img1, flow)
+        # flow, _ = self.flownet(imgs)
+        # flow, _, _ = self.refinenet(img0, img1, flow)
         # c0, c1 = self.refinenet(img0, img1)
 
-        warped_img0 = warp(img0, flow[:, :2])
-        warped_img1 = warp(img1, flow[:, 2:])
+        # warped_img0 = warp(img0, flow[:, :2])
+        # warped_img1 = warp(img1, flow[:, 2:])
 
         points = self.points_fuse(points)
 
-        i0_output = self.cross_tran(points, warped_img0)
+        i0_output = self.cross_tran(points, img0)
         res0 = torch.sigmoid(i0_output)
         # mask0 = torch.sigmoid(i0_output[:, 3:4])
         # merged_img0 = img0 * mask0 + points * (1 - mask0)
         # pred0 = merged_img0 + res0
         # pred0 = torch.clamp(pred0, 0, 1)
 
-        i1_output = self.cross_tran(points, warped_img1)
+        i1_output = self.cross_tran(points, img1)
         res1 = torch.sigmoid(i1_output)
         # mask1 = torch.sigmoid(i1_output[:, 3:4])
         # merged_img1 = img1 * mask1 + points * (1 - mask1)
         # pred1 = merged_img1 + res1
         # pred1 = torch.clamp(pred1, 0, 1)
 
-        x = self.fuse_block(torch.cat([warped_img0, warped_img1, points], dim=1))
+        x = self.fuse_block(torch.cat([img0, img1, points], dim=1))
 
         refine_output = self.transformer(x)
         res = torch.sigmoid(refine_output)
