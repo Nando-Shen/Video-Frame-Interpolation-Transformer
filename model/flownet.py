3329226bf0c703b644aba2ee69ec8cec547df90a
shenjiaming <shenjiaming@rd.netease.com>
2023-02-15 01:01:52 +0800

    sketch training

--- model/flownet.py
+++ model/flownet.py
@@ -16,15 +16,15 @@ try:
 
     from networks.submodules import *
 except:
-    from .networks.resample2d_package.resample2d import Resample2d
-    from .networks.channelnorm_package.channelnorm import ChannelNorm
+    from networks.resample2d_package.resample2d import Resample2d
+    from networks.channelnorm_package.channelnorm import ChannelNorm
 
-    from .networks import FlowNetC
-    from .networks import FlowNetS
-    from .networks import FlowNetSD
-    from .networks import FlowNetFusion
+    from networks import FlowNetC
+    from networks import FlowNetS
+    from networks import FlowNetSD
+    from networks import FlowNetFusion
 
-    from .networks.submodules import *
+    from networks.submodules import *
 'Parameter count = 162,518,834'
 
 class FlowNet2(nn.Module):
