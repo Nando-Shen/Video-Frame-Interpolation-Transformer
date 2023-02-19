d0da0faddb37b612c79adee756225f9b9fd389f3
shenjiaming <shenjiaming@rd.netease.com>
2023-02-19 15:26:16 +0800

    sketch training

--- model/SKETCH.py
+++ model/SKETCH.py
@@ -72,6 +72,9 @@ class SKETCH(nn.Module):
     def forward(self, img0, img1):
         intWidth = img0.shape[2]
         intHeight = img0.shape[1]
+        img0 = torch.cat([img0,img0,img0], dim=1)
+        img1 = torch.cat([img1,img1,img1], dim=1)
+
         # tenPreprocessedOne = img0.view(1, 3, intHeight, intWidth)
         # tenPreprocessedTwo = img1.view(1, 3, intHeight, intWidth)
         intPreprocessedWidth = int(math.floor(math.ceil(intWidth / 64.0) * 64.0))
