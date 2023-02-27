ad5d1ca8a6dc9ff9d32e95687332cb03f8453c9c
shenjiaming <shenjiaming@rd.netease.com>
2023-02-27 16:48:27 +0800

    flow training

--- dataset/atd12k.py
+++ dataset/atd12k.py
@@ -63,7 +63,7 @@ class ATD12k(Dataset):
         # images = [images[i] for i in inputs]
         # imgpaths = [imgpaths[i] for i in inputs]
         # Data augmentation
-        size = (512, 288)
+        size = (448, 256)
         if self.training:
             seed = random.randint(0, 2**32)
             images_ = []
