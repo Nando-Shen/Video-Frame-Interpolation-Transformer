4aa3500f14756a4e7b86406695c9355d7143e54a
shenjiaming <shenjiaming@rd.netease.com>
2023-03-06 23:15:26 +0800

    flow training

--- vis.py
+++ vis.py
@@ -61,7 +61,7 @@ input_tensor = images# Create an input tensor image for your model..
 # Note: input_tensor can be a batch tensor with several images!
 
 # Construct the CAM object once, and then re-use it on many images:
-cam = GradCAM(model=model, target_layers=target_layers, use_cuda=args.use_cuda)
+cam = GradCAM(model=model, target_layers=target_layers, use_cuda=True)
 
 targets = [ClassifierOutputTarget(281)]
 
