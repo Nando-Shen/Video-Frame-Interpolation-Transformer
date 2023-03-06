26f4026b1870a0f414f4d5a8816268d4c37a9783
shenjiaming <shenjiaming@rd.netease.com>
2023-03-06 22:04:52 +0800

    flow training

--- vis.py
+++ vis.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 
 from torch.cuda.amp import autocast, GradScaler
 from torchvision.utils import save_image as imwrite
+from PIL import Image
 
 import config
 import myutils
@@ -33,9 +34,18 @@ args, unparsed = config.get_args()
 device = torch.device('cuda' if args.cuda else 'cpu')
 args.device = device
 args.resume_flownet = False
-train_loader = get_loader('train', args.data_root, args.batch_size, shuffle=True, num_workers=args.num_workers)
+# train_loader = get_loader('train', args.data_root, args.batch_size, shuffle=True, num_workers=args.num_workers)
 test_loader = get_loader('test', args.data_root, args.test_batch_size, shuffle=False, num_workers=args.num_workers)
-
+images, gt, imgpath = next(iter(test_loader))
+# img0 = os.path.join('image', 'frame1.jpg')
+# img1 = os.path.join('image', 'frame3.jpg')
+#
+# points14 = os.path.join('image', 'inter14.jpg')
+# points12 = os.path.join('image', 'inter12.jpg')
+# points34 = os.path.join('image', 'inter34.jpg')
+# gt = os.path.join('image', 'frame2.jpg')
+# data_list = [img0, img1, points14, points12, points34, gt]
+# images = [Image.open(pth) for pth in data_list]
 
 model = VFIformerSmall(args)
 model = torch.nn.DataParallel(model).to(device)
@@ -46,7 +56,7 @@ save_loc = os.path.join(args.checkpoint_dir, "checkpoints")
 load_checkpoint(args, model, optimizer, save_loc + '/model_best1.pth')
 
 target_layers = [model.layer4[-1]]
-input_tensor = # Create an input tensor image for your model..
+input_tensor = images# Create an input tensor image for your model..
 # Note: input_tensor can be a batch tensor with several images!
 
 # Construct the CAM object once, and then re-use it on many images:
@@ -58,7 +68,7 @@ grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
 
 # In this example grayscale_cam has only one image in the batch:
 grayscale_cam = grayscale_cam[0, :]
-visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+visualization = show_cam_on_image(gt, grayscale_cam, use_rgb=True)
 
 for name in model.state_dict():
     print(name)
