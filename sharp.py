0263a7472ca767664c095afb64be788a74db088d
shenjiaming <shenjiaming@rd.netease.com>
2023-03-07 18:38:45 +0800

    flow training

--- sharp.py
+++ sharp.py
@@ -146,10 +146,11 @@ def visualize_grid_to_grid(att_map, grid_index, image, grid_size=8, alpha=0.6):
     if not isinstance(grid_size, tuple):
         grid_size = (grid_size, grid_size)
 
-    H, W = att_map.shape
+    C, H, W = att_map.shape
+    print(att_map.shape)
     with_cls_token = False
 
-    grid_image = highlight_grid(image, [grid_index], grid_size)
+    # grid_image = highlight_grid(image, [grid_index], grid_size)
 
     mask = att_map[grid_index].reshape(grid_size[0], grid_size[1])
     mask = Image.fromarray(mask).resize((image.size))
@@ -157,10 +158,10 @@ def visualize_grid_to_grid(att_map, grid_index, image, grid_size=8, alpha=0.6):
     fig, ax = plt.subplots(1, 2, figsize=(10, 7))
     fig.tight_layout()
 
-    ax[0].imshow(grid_image)
+    ax[0].imshow(image)
     ax[0].axis('off')
 
-    ax[1].imshow(grid_image)
+    ax[1].imshow(image)
     ax[1].imshow(mask / np.max(mask), alpha=alpha, cmap='rainbow')
     ax[1].axis('off')
     plt.show()
@@ -254,8 +255,8 @@ print(list(cache.keys()))
 attention_maps = cache['WindowCrossAttention.forward']
 print(len(attention_maps))
 print(attention_maps[0].shape)
-# visualize_grid_to_grid(attention_maps[4][0,0,:,:], 61, gt)
-visualize_head(attention_maps[7][0,1])
+visualize_grid_to_grid(attention_maps[4][:,0,:,:], 60, gt)
+# visualize_head(attention_maps[7][0,1])
 # for name in model.state_dict():
 #     print(name)
 # print(list(model.modules()))
