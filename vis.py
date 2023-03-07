0923095b93409eb27e87b571229d4379922126b9
shenjiaming <shenjiaming@rd.netease.com>
2023-03-07 15:24:20 +0800

    flow training

--- vis.py
+++ vis.py
@@ -78,7 +78,7 @@ save_loc = os.path.join(args.checkpoint_dir, "checkpoints")
 # load_checkpoint(args, model, optimizer, save_loc + '/model_best1.pth')
 model = torchvision.models.resnet50(pretrained=True)
 model.eval()
-print(*list(model.children()))
+print(*list(model.children())[:-1])
 
 net = ResnetFeatureExtractor(model).to(device)
 
