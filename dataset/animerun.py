aa43c5821489b87485f6d9396897313d52306e5d
shenjiamingnus <e1221616@u.nus.edu>
2024-06-15 00:15:39 +0800

    train on new dataset animeRun

--- dataset/animerun.py
+++ dataset/animerun.py
@@ -58,7 +58,7 @@ class AnimeRun(Dataset):
 
     def __getitem__(self, index):
 
-        imgpaths = [self.data_list[index][0], self.data_list[index][1], self.data_list[index][2], self.data_list[index][3],  self.data_list[index][4],  self.data_list[index][5]]
+        imgpaths = [self.data_list[index][0], self.data_list[index][1], self.data_list[index][2], self.data_list[index][3]]
         # Load images
         images = [Image.open(pth) for pth in imgpaths]
         ## Select only relevant inputs
@@ -68,8 +68,8 @@ class AnimeRun(Dataset):
         # imgpaths = [imgpaths[i] for i in inputs]
         # Data augmentation
         size = (384, 192)
-        flow13 = np.load(self.data_list[index][7]).astype(np.float32)
-        flow31 = np.load(self.data_list[index][8]).astype(np.float32)
+        flow13 = np.load(self.data_list[index][5]).astype(np.float32)
+        flow31 = np.load(self.data_list[index][6]).astype(np.float32)
         flow = [flow13, flow31]
         if self.training:
             seed = random.randint(0, 2**32)
@@ -80,18 +80,18 @@ class AnimeRun(Dataset):
                 images_.append(self.transforms(img_))
             images = images_
 
-            gt = images[5]
+            gt = images[3]
 
-            images = images[:5]
+            images = images[:3]
 
             return images, gt, flow
         else:
             T = self.transforms
             images = [T(img_.resize(size)) for img_ in images]
 
-            gt = images[5]
-            images = images[:5]
-            imgpath = self.data_list[index][6]
+            gt = images[3]
+            images = images[:3]
+            imgpath = self.data_list[index][4]
 
             return images, gt, imgpath, flow
 
