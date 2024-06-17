bfa96f80430f37d2e5c193c3e1c7454a8aade59f
shenjiamingnus <e1221616@u.nus.edu>
2024-06-17 13:07:21 +0800

    ab on vfit

--- dataset/animerun.py
+++ dataset/animerun.py
@@ -34,13 +34,8 @@ class AnimeRun(Dataset):
             img1 = os.path.join(self.data_root, d, 'frame3.jpg')
             # points14 = os.path.join(self.data_root, d, 'frame3.jpg')
 
-            points12 = os.path.join(self.data_root, d, 'inter12.jpg')
             gt = os.path.join(self.data_root, d, 'frame2.jpg')
-
-            region13 = os.path.join(self.region_root, d, 'guide_flo13.npy')
-            region31 = os.path.join(self.region_root, d, 'guide_flo31.npy')
-            # data_list.append([img0, img1, points14, points12, points34, gt, d])
-            data_list.append([img0, img1, points12, gt, d, region13, region31])
+            data_list.append([img0, img1, gt, d])
 
         self.data_list = data_list
 
@@ -58,10 +53,11 @@ class AnimeRun(Dataset):
 
     def __getitem__(self, index):
 
-        imgpaths = [self.data_list[index][0], self.data_list[index][1], self.data_list[index][2], self.data_list[index][3],  self.data_list[index][4],  self.data_list[index][5]]
         imgpaths = [self.data_list[index][0], self.data_list[index][1], self.data_list[index][2], self.data_list[index][3]]
+
         # Load images
         images = [Image.open(pth) for pth in imgpaths]
+
         ## Select only relevant inputs
         # inputs = [int(e)-1 for e in list(self.inputs)]
         # inputs = inputs[:len(inputs)//2] + [3] + inputs[len(inputs)//2:]
@@ -69,13 +65,8 @@ class AnimeRun(Dataset):
         # imgpaths = [imgpaths[i] for i in inputs]
         # Data augmentation
         size = (384, 192)
-        flow13 = np.load(self.data_list[index][7]).astype(np.float32)
-        flow31 = np.load(self.data_list[index][8]).astype(np.float32)
-        flow13 = np.load(self.data_list[index][5]).astype(np.float32)
-        flow31 = np.load(self.data_list[index][6]).astype(np.float32)
-        flow = [flow13, flow31]
         if self.training:
-            seed = random.randint(0, 2**32)
+            seed = random.randint(0, 2 ** 32)
             images_ = []
             for img_ in images:
                 img_ = img_.resize(size)
@@ -83,25 +74,19 @@ class AnimeRun(Dataset):
                 images_.append(self.transforms(img_))
             images = images_
 
-            gt = images[5]
-            gt = images[3]
+            gt = images[2]
+            images = images[:2]
 
-            images = images[:5]
-            images = images[:3]
-
-            return images, gt, flow
+            return images, gt
         else:
             T = self.transforms
             images = [T(img_.resize(size)) for img_ in images]
 
-            gt = images[5]
-            images = images[:5]
-            imgpath = self.data_list[index][6]
-            gt = images[3]
-            images = images[:3]
+            gt = images[2]
+            images = images[:2]
             imgpath = self.data_list[index][4]
 
-            return images, gt, imgpath, flow
+            return images, gt, imgpath
 
     def __len__(self):
         if self.training:
